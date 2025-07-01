50a92456cc2fbfd9e156c7197bdb6afc83bf152d
anthony <anythony20@gmail.com>
2025-07-01 22:01:32 +0000

    Add create method to controller

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -3,6 +3,13 @@
 class Reminders extends Controller {
 
     public function index() {
-        $this->view('reminders/index');
+        $R = $this->model('Reminder');
+        $list_of_reminders = $R->get_all_reminders();
+        $this->view('reminders/index', ['reminders' => $list_of_reminders]);
+    }
+
+    public function create() {
+        $R = $this->model('Reminder');
+        $this->view('reminders/create');
     }
 }
