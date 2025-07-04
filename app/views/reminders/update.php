22310fc4f92ac4fd8a4f2ee866e04bdfdc83286d
anthony <anythony20@gmail.com>
2025-07-04 04:59:53 +0000

    Add bootstrap css styling to create and update reminder forms

--- app/views/reminders/update.php
+++ app/views/reminders/update.php
@@ -1,13 +1,26 @@
 <?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
-  <h2>Update Reminder</h2>
-  <form method="post" action="/reminders/update/<?= $data['reminder']['id'] ?>">
-    <div class="form-group">
-      <label for="subject">Reminder Subject</label><br>
-      <input type="text" id="subject" name="subject" value="<?= ($data['reminder']['subject']) ?>" required>
+<main role="main" class="container">
+  <div class="page-header" id="banner">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1>Update Reminder</h1>
+      </div>
     </div>
-    <br>
-    <button type="submit">Update Reminder</button>
-  </form>
-</div>
+  </div>
+
+  <div class="row">
+    <div class="col-sm-auto">
+      <form method="post" action="/reminders/update/<?= $data['reminder']['id'] ?>">
+        <fieldset>
+          <div class="form-group">
+            <label for="subject">Reminder Subject</label>
+            <input type="text" class="form-control" id="subject" name="subject" value="<?= htmlspecialchars($data['reminder']['subject']) ?>" required>
+          </div>
+          <br>
+          <button type="submit" class="btn btn-primary">Update Reminder</button>
+        </fieldset>
+      </form>
+    </div>
+  </div>
+</main>
 <?php require_once 'app/views/templates/footer.php' ?>
