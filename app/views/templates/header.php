34292b7ada6a1c31499ab90578ecc498f968f64a
anthony <anythony20@gmail.com>
2025-07-04 04:38:35 +0000

    Add reminder navigation to the header dropdown

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -30,14 +30,12 @@ if (!isset($_SESSION['auth'])) {
           <a class="nav-link" href="/about">About Me</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+          <a class="nav-link dropdown-toggle" href="/reminders" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Reminders
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="/reminders">View Reminders</a></li>
+            <li><a class="dropdown-item" href="/reminders/create">Create Reminder</a></li>
           </ul>
         </li>
         <li class="nav-item">
